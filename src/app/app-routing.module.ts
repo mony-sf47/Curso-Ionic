25c19acd9e65bbc66025a8dd549f8e99c3f1ad76
Rubén Sánchez <luis.sanchezma@uanl.mx>
2019-04-03 11:28:11 -0600

    Sesion 2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,11 @@
-import { NgModule } from '@angular/core';
-import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { NgModule } from "@angular/core";
+import { PreloadAllModules, RouterModule, Routes } from "@angular/router";
 
 const routes: Routes = [
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'home',
-    loadChildren: './home/home.module#HomePageModule'
-  },
-  {
-    path: 'list',
-    loadChildren: './list/list.module#ListPageModule'
+    path: "",
+    redirectTo: "catalog",
+    pathMatch: "full"
   }
 ];
 
